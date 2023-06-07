87fcc98e8775e2206bdaab15632ef73724b4861b
Bauyrzhan Tursyn <bauyrzhan.tursyn@uco.kz>
2023-06-07 23:18:12 +0600

    solution

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task4;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println(Character.toChars(99));
+        System.out.println(Character.toChars(104));
+        System.out.println(Character.toChars(97));
+        System.out.println(Character.toChars(114));
+
     }
 }
